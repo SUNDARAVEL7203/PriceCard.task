35b40bf1ae31cf3dc1ef76a18ca1938cac3a545c
SUNDARAVEL7203 <sundarvel.kasi@gmail.com>
2024-08-26 16:10:00 +0530

    PriceCard.task

---
+++ shoppingcart/src/Components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+
+const Footer = () => {
+  return (
+    <div className='py-3 bg-secondary d-flex justify-content-center'>
+        <p className='fw-bolder text-white'>&copy; Sundaravel</p>
+    </div>
+  )
+}
+
+export default Footer
